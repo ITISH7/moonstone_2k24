cfd7fcbe6510b88df9196396f3853e41cd57025d
jayesh12 <jayeshtapdiya@gmail.com>
2024-03-20 22:40:41 +0530

    sponsor image added push

--- src/pages/Sponsor.jsx
+++ src/pages/Sponsor.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import classes from './Sponsor.module.css'
+import img from '../assets/sponsor.png'
 
 const Sponsor = () => {
     return (
         <>
             <section id="about" className={classes.aboutSec}>
+                <h2 className={classes.heading1} >Sponsor</h2>
                 <div className={classes.about}>
+
+
                     <div className={classes.details}>
                         {/* <h3 className={classes.heading}>EMPOWER YOUR FUTURE </h3> */}
-                        <h2 className={classes.heading1} >Coming Soon!!!!</h2>
+                        <img src={img} alt="Sponsor Image" srcset="" className={classes.spo} />
                     </div>
                 </div>
             </section>
