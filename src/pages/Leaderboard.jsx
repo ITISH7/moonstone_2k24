54503cf5cc00904e9580b88aa061ec1d69616b52
ITISH <itishjain7@gmail.com>
2024-10-11 09:40:24 +0000

    added leaderboard images

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -3,83 +3,17 @@ import classes from "./Leaderboard.module.css";
 
 const Leaderboard = () => {
   const data = [
+    
     {
       id: 1,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709881740/IMG-20240308-WA0000_aplgg7.jpg",
-      name: "Sanket Gupta",
-      pos: "Deployment Expert & Guide Assistant Prof.CSE, MU",
-      linkdin: "https://www.linkedin.com/in/sanket-gupta-04a11895/",
-      gmail: "sanket.gupta@medicaps.ac.in",
-      // github: "github",
-    },
-    {
-      id: 2,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709800591/IMG-20240307-WA0013_xywfmd.jpg",
+      image: "https://res.cloudinary.com/dejlssoxx/image/upload/v1728639429/Itish_Jain_vmudtl.jpg",
       name: "Itish Jain",
-      pos: "MERN",
+      pos: "Full stack Developer",
       linkdin: "https://www.linkedin.com/in/itish-jain-a3479822a/",
       gmail: "mailto:itishjain7@gmail.com",
       github: "https://github.com/ITISH7",
     },
-    {
-      id: 3,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709797956/IMG-20240307-WA0005_fjxate.jpg",
-      name: "Jayesh Tapdiya",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/jayesh-tapdiya-01573024a/",
-      gmail: "mailtto:jayesh121220@gmail.com",
-      github: "https://github.com/jayeshtapdiya",
-    },
-    {
-      id: 4,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709790198/f18fa73a-ac60-4623-a31d-666bfab12598_s5fnfa.jpg",
-      name: "Khushi Sharma",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/khushi-sharma-74a218224/",
-      gmail: "mailto:khushidksharma@gmail.com",
-      github: "https://github.com/Khushisharma23",
-    },
-    {
-      id: 5,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709800072/IMG-20240307-WA0009_oabpu6.jpg",
-      name: "Manas Choudhary",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/manas-choudhary-019411226/",
-      gmail: "mailto:mannchoudhary3431@gmail.com",
-      github: "https://github.com/ManasGitRep",
-    },
-    {
-      id: 6,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709800069/IMG-20240307-WA0011_w9vugo.jpg",
-      name: "Pranay Mishra",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/pranay-mishra-0189b1248/",
-      gmail: "mailto:mishrapranay21@gmail.com",
-      github: "https://github.com/Work-from-Pranay",
-    },
-   
-
-    {
-      id: 7,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709800071/IMG-20240307-WA0010_rxotgn.jpg",
-      name: "Madhvendra Singh Bais",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/madhvendra-singh-bais-a537a0238/",
-      gmail: "mailto:madhvebais2002@gmail.com",
-      github: "https://github.com/madhavbais",
-    },
     
-
-    
-    {
-      id: 8,
-      image: "https://res.cloudinary.com/dulzutyqm/image/upload/v1709800068/IMG-20240307-WA0012_qmrzka.jpg",
-      name: "Mahak Hardiya",
-      pos: "MERN",
-      linkdin: "https://www.linkedin.com/in/mahak-hardiya-650aa5230/",
-      gmail: "mailto:mahakhardiya@gmailcom",
-      github: "https://github.com/mahakhardiya",
-    },
 
   ];
 
@@ -104,17 +38,17 @@ const Leaderboard = () => {
                   <div className={classes.social_links}>
                     {/* linkdin */}
                     <a href={ele.linkdin} target="_blank" rel="noopener noreferrer" className={classes.footer_a_link}>
-                      <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709616689/linkedin-circled--v1_jignzr.png " alt="" />
+                      <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728639546/linkdinlogo_rnmy8w.jpg " alt="" />
                     </a>
 
                     {/* gamil */}
                     <a href={ele.gmail} target="_blank" rel="noopener noreferrer" className={classes.footer_a_link}>
-                      <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709617445/t_500x300_znjzks.jpg " alt="" />
+                      <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728639546/gmaillogo_e7ddsz.png " alt="" />
                     </a>
 
                     {/* github */}
                     <a href={ele.github} target="_blank" rel="noopener noreferrer" className={classes.footer_a_link}>
-                      <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709617630/icons8-github-30_l91dpj.png  " alt="" />
+                      <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728639545/githublogo_tk1oij.jpg " alt="" />
                     </a>
                   </div>
                 </div>
