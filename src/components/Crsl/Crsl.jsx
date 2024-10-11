b6e1dca2c1bbfe48480b1c9fd3b0f5be376b5421
ITISH <itishjain7@gmail.com>
2024-10-11 08:40:24 +0000

    added images in crsl

--- src/components/Crsl/Crsl.jsx
+++ src/components/Crsl/Crsl.jsx
@@ -5,18 +5,18 @@ const Crsl = () => {
   return (
     <div className={classes.scroller}>
                 <div className={classes.scroller_in}>
-                    <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img3_un2khg.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709574569/img17_xxarsz.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542844/img34_hiqhan.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462521889_878567417310911_4536411965012835692_n_pmpmlh.jpg" alt="" />
+                    <img src="https://asset.cloudinary.com/dejlssoxx/d6866f1f8c2a016e6bd2dd70d1e7a0f9" alt="" />
+                    {/* <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542844/img34_hiqhan.jpg" alt="" />
                     <img src="https://moonstone.onrender.com/images/13.webp" alt="" />
                     <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img12_waemq3.jpg" alt="" />
                     <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542831/img6_yczbbm.jpg" alt="" />
                     <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/5.%20Sports_11zon.jpg" alt="" />
                     <img src="https://moonstone.onrender.com/images/10.jpg" alt="" />
-                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/2b.%20Life@MU_11zon.jpg" alt="" />
+                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/2b.%20Life@MU_11zon.jpg" alt="" /> */}
                 </div>
                 <div className={classes.scroller_in}>
-                <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img3_un2khg.jpg" alt="" />
+                {/* <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img3_un2khg.jpg" alt="" />
                 <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709574569/img17_xxarsz.jpg" alt="" />
                     <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542844/img34_hiqhan.jpg" alt="" />
                     <img src="https://moonstone.onrender.com/images/13.webp" alt="" />
@@ -24,7 +24,7 @@ const Crsl = () => {
                     <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542831/img6_yczbbm.jpg" alt="" />
                     <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/5.%20Sports_11zon.jpg" alt="" />
                     <img src="https://moonstone.onrender.com/images/10.jpg" alt="" />
-                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/2b.%20Life@MU_11zon.jpg" alt="" />
+                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/2b.%20Life@MU_11zon.jpg" alt="" /> */}
                 </div>
     </div>
   )
