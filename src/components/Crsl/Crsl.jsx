21a18eb2da11f0b61464135fd91ac952ad88873f
ITISH <itishjain7@gmail.com>
2024-10-11 08:46:53 +0000

    added more links in crsl

--- src/components/Crsl/Crsl.jsx
+++ src/components/Crsl/Crsl.jsx
@@ -6,14 +6,14 @@ const Crsl = () => {
     <div className={classes.scroller}>
                 <div className={classes.scroller_in}>
                     <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462521889_878567417310911_4536411965012835692_n_pmpmlh.jpg" alt="" />
-                    <img src="https://asset.cloudinary.com/dejlssoxx/d6866f1f8c2a016e6bd2dd70d1e7a0f9" alt="" />
-                    {/* <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542844/img34_hiqhan.jpg" alt="" />
-                    <img src="https://moonstone.onrender.com/images/13.webp" alt="" />
-                    <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img12_waemq3.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542831/img6_yczbbm.jpg" alt="" />
-                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/5.%20Sports_11zon.jpg" alt="" />
-                    <img src="https://moonstone.onrender.com/images/10.jpg" alt="" />
-                    <img src="https://www.medicaps.ac.in/resources/img/For-Students--img/Life@MU/2b.%20Life@MU_11zon.jpg" alt="" /> */}
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462142924_18268831633222299_2047894172731655028_n_ebhwea.webp" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462704594_1865413090620371_8143185432720928837_n_tglyjm.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_453006994_804070425229944_2475966683786671266_n_hafnnd.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_454583855_812621384374848_8819387516437269943_n_uyml85.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_459968342_1017433573212864_2016697063859775758_n_hsg7ip.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159954_791755479794772_1975277240853404444_n_mrfcn0.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635810/SaveClip.App_450159455_791755429794777_7116189251322814032_n_pcgvxx.jpg" alt="" />
+                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159428_791755459794774_8527030572328470829_n_do5svl.jpg" alt="" />
                 </div>
                 <div className={classes.scroller_in}>
                 {/* <img src="https://res.cloudinary.com/dulzutyqm/image/upload/v1709542830/img3_un2khg.jpg" alt="" />
