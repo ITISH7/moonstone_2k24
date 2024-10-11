ea153f89ddc007f9c468c6e1c08480313f2953b4
ITISH <itishjain7@gmail.com>
2024-10-11 08:58:10 +0000

    added new changes in crsl

--- src/components/Crsl/Crsl.jsx
+++ src/components/Crsl/Crsl.jsx
@@ -4,29 +4,29 @@ import classes from "./Crsl.module.css";
 const Crsl = () => {
   return (
     <div className={classes.scroller}>
-                <div className={classes.scroller_in}>
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462521889_878567417310911_4536411965012835692_n_pmpmlh.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462142924_18268831633222299_2047894172731655028_n_ebhwea.webp" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462704594_1865413090620371_8143185432720928837_n_tglyjm.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_453006994_804070425229944_2475966683786671266_n_hafnnd.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_454583855_812621384374848_8819387516437269943_n_uyml85.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_459968342_1017433573212864_2016697063859775758_n_hsg7ip.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159954_791755479794772_1975277240853404444_n_mrfcn0.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635810/SaveClip.App_450159455_791755429794777_7116189251322814032_n_pcgvxx.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159428_791755459794774_8527030572328470829_n_do5svl.jpg" alt="" />
-                </div>
-                <div className={classes.scroller_in}>
-                <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434335202_1565380737615330_382867321566186990_n_wfbpa5.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434362512_399532576360733_1883271924429448149_n_gmquvz.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434263124_261185910390054_5085116166518271731_n_kaypzd.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_452195160_799904722313181_6453146402425761982_n_b3bkhw.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434561934_786970433340015_3565513365893367969_n_a7j6yu.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_452098112_799904695646517_7443917146098365544_n_t2jhzw.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636833/SaveClip.App_453976828_808300371473616_3876492190915780622_n_yuoano.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_453846537_808300384806948_7458678361663714931_n_nvljeo.jpg" alt="" />
-                    <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450147023_791755416461445_8337933301269881048_n_mopfls.jpg" alt="" />
-                </div>
-                </div>
+      <div className={classes.scroller_in}>
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462521889_878567417310911_4536411965012835692_n_pmpmlh.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462142924_18268831633222299_2047894172731655028_n_ebhwea.webp" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_462704594_1865413090620371_8143185432720928837_n_tglyjm.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_453006994_804070425229944_2475966683786671266_n_hafnnd.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_454583855_812621384374848_8819387516437269943_n_uyml85.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635812/SaveClip.App_459968342_1017433573212864_2016697063859775758_n_hsg7ip.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159954_791755479794772_1975277240853404444_n_mrfcn0.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635810/SaveClip.App_450159455_791755429794777_7116189251322814032_n_pcgvxx.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450159428_791755459794774_8527030572328470829_n_do5svl.jpg" alt="" />
+      </div>
+      <div className={classes.scroller_in}>
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434335202_1565380737615330_382867321566186990_n_wfbpa5.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434362512_399532576360733_1883271924429448149_n_gmquvz.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434263124_261185910390054_5085116166518271731_n_kaypzd.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_452195160_799904722313181_6453146402425761982_n_b3bkhw.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636832/SaveClip.App_434561934_786970433340015_3565513365893367969_n_a7j6yu.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_452098112_799904695646517_7443917146098365544_n_t2jhzw.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636833/SaveClip.App_453976828_808300371473616_3876492190915780622_n_yuoano.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728636834/SaveClip.App_453846537_808300384806948_7458678361663714931_n_nvljeo.jpg" alt="" />
+        <img src="https://res.cloudinary.com/dejlssoxx/image/upload/v1728635811/SaveClip.App_450147023_791755416461445_8337933301269881048_n_mopfls.jpg" alt="" />
+      </div>
+
     </div>
   )
 }
